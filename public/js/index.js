4dcb6356de8b7cf0eba7ad07969804a102090fc9
Jared Flasch <jflasch@mp.ics.com>
2018-10-26 10:52:04 -0500

    Emit new message on createMessage

--- public/js/index.js
+++ public/js/index.js
@@ -2,11 +2,6 @@ var socket = io();
 
 socket.on('connect', function () {
     console.log('Connected to server');
-
-    socket.emit('createMessage', {
-        from: 'Collin',
-        text: 'This is a client message from Collin'
-    });
 });
 
 socket.on('newMessage', function (message) {
