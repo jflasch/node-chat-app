1412d4e1be0aae1e03daaf18bff5a41d055d8978
Jared Flasch <jflasch@mp.ics.com>
2018-10-26 13:33:06 -0500

    Added geolocation functionality

--- server/utils/message.test.js
+++ server/utils/message.test.js
@@ -1,6 +1,6 @@
 var expect = require('expect');
 
-var {generateMessage} = require('./message');
+var {generateMessage, generateLocationMessage} = require('./message');
 
 describe('generateMessage', () => {
     it('should generate the correct message object', () => {
@@ -9,4 +9,14 @@ describe('generateMessage', () => {
         expect(message.from).toBe('Jared');
         expect(typeof message.createdAt).toBe('number');
     })
+});
+
+describe('generateLocationMessage', () => {
+    it('should generate the correct location object', () => {
+        var message = generateLocationMessage('Jared', 30, 50);
+        var testURL = 'https://www.google.com/maps?q=30,50';
+        expect(message.from).toBe('Jared');
+        expect(message.url).toBe(testURL);
+        expect(typeof message.createdAt).toBe('number');
+    });
 });
