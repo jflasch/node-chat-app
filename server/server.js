8d11b6c7bd5b9637145898f40a5aa8c828097e05
Jared Flasch <jflasch@mp.ics.com>
2018-10-29 09:50:05 -0500

    Rooms are now case insensitive

--- server/server.js
+++ server/server.js
@@ -20,16 +20,18 @@ io.on('connection', (socket) => {
     console.log('New user connected');
 
     socket.on('join', (params, callback) => {
-        if (!isRealString(params.name) || !isRealString(params.room)) {
+        var room = params.room.toLowerCase();
+        if (!isRealString(params.name) || !isRealString(room)) {
             return callback('Name and room name are required');
         }
-        socket.join(params.room);
+        socket.join(room);
+        console.log(room);
         users.removeUser(socket.id);
-        users.addUser(socket.id, params.name, params.room);
+        users.addUser(socket.id, params.name, room);
 
-        io.to(params.room).emit('updateUserList', users.getUserList(params.room));
+        io.to(room).emit('updateUserList', users.getUserList(room));
         socket.emit('newMessage', generateMessage('Admin', "Welcome to the chat app!"));
-        socket.broadcast.to(params.room).emit('newMessage', generateMessage('Admin', `${params.name} has joined the chat room.`));
+        socket.broadcast.to(room).emit('newMessage', generateMessage('Admin', `${params.name} has joined the chat room.`));
 
         callback();
     });
